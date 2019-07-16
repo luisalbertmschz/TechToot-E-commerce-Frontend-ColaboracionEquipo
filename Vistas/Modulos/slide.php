c334f3dccd9a23e98b2bc1e2298bddf8b8a15a10
Albert <cerebrito07@hotmail.com>
2019-07-16 14:10:37 -0400

    Convirtiendo el Slide en un módulo dinámico, ahora mismo ya la información de la cual se alimenta el slide viene directamente de lo que contiene la base de datos, en la tabla Slide está toda la información referente a cada slide lo que permite gestionar el contenido del  mismo estableciendo actualizaciones de dichos valores desde la base de datos.

--- Vistas/Modulos/slide.php
+++ Vistas/Modulos/slide.php
@@ -12,10 +12,70 @@ SLIDESHOW
 
 
             <ul>
+
+
+               <?php
+
+                $slide = ControladorSlide::ctrMostrarSlide();
+
+                foreach($slide as $key => $value) {
+
+                    $estiloImgProducto = json_decode($value["estiloImgProducto"], true);
+                    $estiloTextoSlide = json_decode($value["estiloTextoSlide"], true);
+
+                    $titulo1 = json_decode($value["titulo1"], true);
+                    $titulo2 = json_decode($value["titulo2"], true);
+                    $titulo3 = json_decode($value["titulo3"], true);
+
+
+
+
+                    /* var_dump($slide); */
+
+                    /* var_dump($estiloImgProducto); */
+
+                        echo '<li>
+
+                        <img src="http://localhost/E-commerce-Backend/'.$value["imgFondo"].'">
+
+                        <div class="slideOpciones '.$value["tipoSlide"].'">
+
+                            <img class="imgProducto"src="http://localhost/E-commerce-Backend/'.$value["imgProducto"].'"style="top:'.$estiloImgProducto["top"].'; right: '.$estiloImgProducto["right"].'; width: '.$estiloImgProducto["width"].'; left: '.$estiloImgProducto["left"].';">
+
+                            <div class="textosSlide"style="top:'.$estiloTextoSlide["top"].' ;left:'.$estiloTextoSlide["left"].'; width:'.$estiloTextoSlide["width"].'; right: '.$estiloTextoSlide["right"].';">
+
+                                <h1 style="color:'.$titulo1["color"].'">'.$titulo1["texto"].'</h1>
+                                <h2 style="color:'.$titulo2["color"].'">'.$titulo2["texto"].'</h2>
+                                <h3 style="color:'.$titulo3["color"].'">'.$titulo3["texto"].'</h3>
+                                
+                                
+
+                                    <a href="'.$value["url"].'">
+
+                                    <!--  Fue elimnada la clase btn default que aplica el color global del site, para poder entonces establecer colores predefinidos si así se requiere -->
+
+                                    '.$value["botonSlide"].'
+                                    
+                                    
+                                    </a>
+                            
+                            
+                            
+                            </div>
+                        
+                        
+                        </div>
+
+                    </li>'; 
+
+               }
+
+
+               ?>
             
                <!-- SLIDE NUMERO 1 -->
                 
-                <li>
+<!--                 <li>
 
                      <img src="http://localhost/E-commerce-Backend/Vistas/img/slide/default/back_default.jpg">
 
@@ -23,19 +83,18 @@ SLIDESHOW
 
                        <img class="imgProducto"src="http://localhost/E-commerce-Backend/Vistas/img/slide/slide1/HPJET8035PRO.png"style="top: 15%; right: 10%; width: 35%;">
 
-                        <div class="textosSlide"style="top:30% ;left:10%; ">
+                        <div class="textosSlide"style="top:30% ;left:10%; width:40%;">
 
-                            <h1 style="">HP OfficeJet Pro 8035</h1>
+                            <h1 style="color:">HP OfficeJet Pro 8035</h1>
                             <h2 style="">Diseñada para ahorrarte tiempo</h2>
-                            <h2 style="right:50px; color:#BB002F; font-weight:bold;">Desde $149.99 Dólares</h2>
-                            <img class=""src="http://localhost/E-commerce-Backend/Vistas/img/slide/slide1/HPLOG.png"style="top:300px; width:130px; left:-19px;"> <br>
+                            <h2 style=" color:#BB002F; font-weight:bold;">Desde $149.99 Dólares</h2>
                             
 
-                                <a href="">
+                                <a href=""> -->
 
                                <!--  Fue elimnada la clase btn default que aplica el color global del site, para poder entonces establecer colores predefinidos si así se requiere -->
 
-                                <button class="btn btn-default backColor  text-uppercase">
+<!--                                 <button class="btn btn-default backColor  text-uppercase">
                                 Comprar ahora <span class="fa fa-chevron-right"></span>
                                 </button>
                                 
@@ -50,11 +109,12 @@ SLIDESHOW
                      </div>
 
                 </li>
-
+ -->
 
 
                <!--  SLIDE NUMERO 2 -->
-                <li>
+
+                <!-- <li>
 
                      <img src="http://localhost/E-commerce-Backend/Vistas/img/slide/default/back_default.jpg">
 
@@ -72,7 +132,7 @@ SLIDESHOW
                                 <a href="">
 
                                 <button class="btn btn-default backColor text-uppercase">
-                                Conocer más   <span class="fa fa-chevron-right"></span>
+                                Conocer más<span class="fa fa-chevron-right"></span>
                                 </button>
                                 
                                 </a>
@@ -83,24 +143,24 @@ SLIDESHOW
                      </div>
 
                 </li>
-
+ -->
                 <!-- SLIDE NUMERO 3 -->
                 
-                <li>
+                <!-- <li>
 
                      <img src="http://localhost/E-commerce-Backend/Vistas/img/slide/default/back_default.jpg">
 
                      <div class="slideOpciones slideOpcion2">
 
-                       <img class="imgProducto"src="http://localhost/E-commerce-Backend/Vistas/img/slide/slide3/RN7PRO1.png" style="top: 30px; left: 2%; width: 45%; opacity: 1;" >
+                       <img class="imgProducto"src="http://localhost/E-commerce-Backend/Vistas/img/slide/slide3/RN7PRO1.png" style="top: 30%; left: 2%; width: 45%; opacity: 1;" > -->
                        <!--TODO:POSTCION  RIGHT  de imagenes -->
                       
-                        <div class="textosSlide" style="top:15% ;right:10%;" >
+                       <!--  <div class="textosSlide" style="top:15% ;right:5%;" >
 
                             <h1 style="">Xiaomi Redmi Note 7 Pro</h1>
                             <h2 style="">Cámara dual de 48 + 5 MP con IA</h2>
-                            <h2 style="right:50px;">Una nueva experiencia a nivel de un flagship</h2>
-                            <h3 style="right:50px; color:#BB002F; font-weight:bold;">Desde $ 201.74 Dólares</h3>
+                            <h2 style="">Una nueva experiencia a nivel de un flagship</h2>
+                            <h3 style=" color:#BB002F; font-weight:bold;">Desde $ 201.74 Dólares</h3>
 
                                 <a href="">
 
@@ -118,25 +178,25 @@ SLIDESHOW
                      
                      </div>
 
-                </li>
+                </li> -->
 
 
                 <!-- SLIDE NUMERO 4 -->
                 
-                <li>
+               <!--  <li>
 
                      <img src="http://localhost/E-commerce-Backend/Vistas/img/slide/default/back_default.jpg">
 
                      <div class="slideOpciones slideOpcion2">
 
-                       <img class="imgProducto"src="http://localhost/E-commerce-Backend/Vistas/img/slide/slide4/SURFB4PRO.png" style="top: 30px; left: 2%; width: 45%; opacity: 1;">
+                       <img class="imgProducto"src="http://localhost/E-commerce-Backend/Vistas/img/slide/slide4/SURFB4PRO.png" style="top: 30%; left: 2%; width: 45%; opacity: 1;">
 
                         <div class="textosSlide" style="top:30% ;right:10%;">
 
                             <h1 style="">Conoce la Surface Book 2 Pro</h1>
                             <h2 style="">Liviana, versatil y potente</h2>
-                            <h3 style="right:50px; color:#BB002F; font-weight:bold;">Desde $ 2,479 Dólares</h3>
-                            <h3 style=""></h3> 
+                            <h3 style=" color:#BB002F; font-weight:bold;">Desde $ 2,479 Dólares</h3>
+            
                                 <a href="">
                                 <button class="btn btn-default backColor text-uppercase">
                                 Comprar ahora <span class="fa fa-chevron-right"></span>
@@ -148,7 +208,7 @@ SLIDESHOW
 
                      </div>
 
-                </li>
+                </li> -->
 
             
             </ul>
